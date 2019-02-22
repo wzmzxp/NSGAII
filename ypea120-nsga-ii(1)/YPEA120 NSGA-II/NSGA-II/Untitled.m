1c6014da94fe950c0e36a118d220e3cc9d673e61
wzmzxp <2581806204@qq.com>
2019-02-23 03:13:27 +0800

    删除了多余
    删了注释

--- ypea120-nsga-ii(1)/YPEA120 NSGA-II/NSGA-II/Untitled.m
+++
@@ -1,3 +0,0 @@
-for i=1:50
-a=num2str(F1(i).Cost)
-end
--- ypea120-nsga-ii(1)/YPEA120 NSGA-II/NSGA-II/nsga2.m
+++ ypea120-nsga-ii(1)/YPEA120 NSGA-II/NSGA-II/nsga2.m
@@ -1,15 +1,4 @@
-%
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-%
-% Project Code: YPEA120
-% Project Title: Non-dominated Sorting Genetic Algorithm II (NSGA-II)
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
+
 
 clc;
 clear;
@@ -170,7 +159,7 @@ end
 index1=[];
 t1=size(index);
 for i4=2:t1(2)
-    if index(i4).Cost(2)<=0
+    if index(i4).Cost(2)<=0.08
         index1=[index1,index(i4)];
     end
 end
